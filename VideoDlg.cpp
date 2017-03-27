e1e731493c936e8e1d437883f72cc100e7c5b4d6
toiletduck <gaolijun@sina.com>
2017-03-27 12:18:08 +0800

    视频显示窗口需要增加的代码

--- VideoDlg.cpp
+++ VideoDlg.cpp
@@ -5,6 +5,8 @@
 #include "BarClientDemo.h"
 #include "VideoDlg.h"
 #include "afxdialogex.h"
+#include "AdManager.h"
+#include "player.h"
 
 
 // CVideoDlg 对话框
@@ -14,7 +16,7 @@ IMPLEMENT_DYNAMIC(CVideoDlg, CDialogEx)
 CVideoDlg::CVideoDlg(CWnd* pParent /*=NULL*/)
 	: CDialogEx(IDD_VIDEODLG, pParent)
 {
-
+	m_pPlayer = NULL;
 }
 
 CVideoDlg::~CVideoDlg()
@@ -28,7 +30,61 @@ void CVideoDlg::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CVideoDlg, CDialogEx)
+	ON_MESSAGE(WM_APP_PLAYER_EVENT, &CVideoDlg::OnPlayerEvent)
+	ON_WM_SIZE()
+	ON_WM_PAINT()
 END_MESSAGE_MAP()
 
 
 // CVideoDlg 消息处理程序
+
+LRESULT CVideoDlg::OnPlayerEvent(WPARAM pUnkPtr, LPARAM lp)
+{
+	HRESULT hr = m_pPlayer->HandleEvent(pUnkPtr);
+	if (FAILED(hr))
+	{
+	}
+
+	return 0;
+}
+
+BOOL CVideoDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	AdManager& adManager = AdManager::getInstance();
+	m_pPlayer = adManager.setVideoWnd(m_hWnd);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 异常: OCX 属性页应返回 FALSE
+}
+
+
+void CVideoDlg::OnSize(UINT nType, int cx, int cy)
+{
+	CDialogEx::OnSize(nType, cx, cy);
+
+	if (m_pPlayer)
+	{
+		m_pPlayer->ResizeVideo(cx, cy);
+	}
+}
+
+
+void CVideoDlg::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+
+	if (m_pPlayer && m_pPlayer->HasVideo())
+	{
+		// Video is playing. Ask the player to repaint.
+		m_pPlayer->Repaint();
+	}
+	else
+	{
+		// The video is not playing, so we must paint the application window.
+		RECT rc;
+		GetClientRect(&rc);
+		FillRect(dc, &rc, (HBRUSH)COLOR_WINDOW);
+	}
+}
--- VideoDlg.h
+++ VideoDlg.h
@@ -2,6 +2,7 @@
 
 
 // CVideoDlg 对话框
+class CPlayer;
 
 class CVideoDlg : public CDialogEx
 {
@@ -10,6 +11,7 @@ class CVideoDlg : public CDialogEx
 public:
 	CVideoDlg(CWnd* pParent = NULL);   // 标准构造函数
 	virtual ~CVideoDlg();
+	CPlayer *m_pPlayer;
 
 // 对话框数据
 #ifdef AFX_DESIGN_TIME
@@ -20,4 +22,9 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 
 	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnPaint();
+	afx_msg LRESULT OnPlayerEvent(WPARAM pUnkPtr, LPARAM lp);
 };
